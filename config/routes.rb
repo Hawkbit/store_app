a348dc9183f95d75512c1352750059f057d0070a
Shawn Shrader <hawkbit@gmail.com>
2015-02-20 23:15:20 -0800

    Pages built and somewhat functional. See README for known error docs.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
 
-  get 'pages/index'
+  root 'pages#index'
 
-  get 'pages/tips'
-
-  get 'pages/contact'
-
-  get 'pages/about'
-
-  get 'welcome/index'
+  get 'index' 	=> 'pages#index' 
+  get 'tips'		=> 'pages#tips'
+  get 'contact'	=> 'pages#contact'
+  get 'about'		=> 'pages#about'
+  get 'welcome/index'	=> 'welcome#index'
+  get 'products'	=> 'products#index'
 
   resources :products
 
-  root 'pages#index'
+
 
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,25 +4,25 @@ class PagesControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
-    assert_select "title", "Home - Lugnuts"
+    assert_select "title", "Home - Lugnuts Auto Parts"
   end
 
   test "should get tips" do
     get :tips
     assert_response :success
-    assert_select "title", "Tips - Lugnuts"
+    assert_select "title", "Tips - Lugnuts Auto Parts"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact - Lugnuts"
+    assert_select "title", "Contact - Lugnuts Auto Parts"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About - Lugnuts"
+    assert_select "title", "About - Lugnuts Auto Parts"
   end
 
 end
