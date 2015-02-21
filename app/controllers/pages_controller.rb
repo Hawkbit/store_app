079a380ab907b344905200f699cd29a68afe0a50
Shawn Shrader <hawkbit@gmail.com>
2015-02-20 19:42:04 -0800

    Added Pages controller for about, contact, tips and index

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def tips
+  end
+
+  def contact
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'pages/index'
+
+  get 'pages/tips'
+
+  get 'pages/contact'
+
+  get 'pages/about'
+
   resources :products
 
   root 'welcome#index'
