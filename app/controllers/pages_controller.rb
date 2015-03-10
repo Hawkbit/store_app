9388f4c77a3ac41ede15109d2d97273641da4a39
HowardSnowden <therealdpyles@gmail.com>
2015-03-10 19:35:41 +0100

    Updated cart functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,18 @@
 class PagesController < ApplicationController
+  
   def index
+    @cart = current_cart
   end
 
   def tips
+    @cart = current_cart
   end
 
   def contact
+    @cart = current_cart
   end
 
   def about
+      @cart = current_cart
   end
 end
