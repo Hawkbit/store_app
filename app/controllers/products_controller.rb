b6a43e4dbf27f2bab6b4fe61cd485db34cf84470
HowardSnowden <therealdpyles@gmail.com>
2015-03-19 02:18:51 +0100

    Added heading logic for product listings

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,12 +4,14 @@ class ProductsController < ApplicationController
        if params[:productLine]
         
 		@products = Product.where("\"productLine\" = ?", params[:productLine])
-        
+        @title = "Showing category '#{params[:productLine]}'"
+           
         elsif params[:search]
         @products = Product.search(params[:search])
-        
+        @title = "Your search for '#{params[:search]}' returned #{@products.count} result(s)"
         else
            @products = Product.all
+           @title =  "Showing all products"
         end
         @cart = current_cart
 	end
