dfb70c37a8f7e6e437a7c6b23652e1c7c10fb622
Shawn Shrader <hawkbit@gmail.com>
2015-03-30 17:31:15 -0700

    updated registrations_controller (thx Damien)

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController  
     respond_to :json
     
+    before_filter :assign_variable
+
+	private
+	def assign_variable
+   	@cart = current_cart
+	end
+    
 end 
