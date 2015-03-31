807fd9e9d9d8e829f8a3fdfa8541890f1d6efba9
HowardSnowden <therealdpyles@gmail.com>
2015-03-31 06:03:09 +0100

    sync

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,13 @@ class RegistrationsController < Devise::RegistrationsController
     
     respond_to :json
     
+  before_filter :assign_variable
+
+	private
+	def assign_variable
+   	@cart = current_cart
+	end
+    
 
 
     
