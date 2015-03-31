6e2d863684dcf02639f44604c69c9563ff176fbd
HowardSnowden <therealdpyles@gmail.com>
2015-03-31 05:52:13 +0100

    hello

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,15 @@
 class RegistrationsController < Devise::RegistrationsController  
+    
+    before_filter :assign_variable
+    
     respond_to :json
+    
+    private 
+    
+    def assign_variable
+        @cart = current_cart
+    
+    end
+    
     
 end 
