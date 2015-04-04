db83dc1337341fda0a5191846ef2f229060935f6
Shawn Shrader <hawkbit@gmail.com>
2015-04-04 08:35:52 -0700

    changed pay method from check/paypal to stripe

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ActiveRecord::Base
    has_many :line_items, dependent: :destroy
     
-    PAYMENT_TYPES = ["Paypal", "Credit card" ]
+    PAYMENT_TYPES = [ "Stripe" ]
     
     validates :name, :address, :email, presence: true
     validates :pay_type, inclusion: PAYMENT_TYPES
